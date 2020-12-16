8e262369c63ee5dfd90502ca2c7091c3cdc15fc7
LukeMcCann <developer-lukemccann@outlook.com>
2020-12-16 20:32:02 +0000

    Reduce number of todos in generated data

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 final class TodosController extends Controller
 {
     public function index()
     {
-        return view('todos.index');
+        $todos = Todo::all();
+
+        return view('todos.index')->with('todos', $todos);
     }
 }
--- database/seeders/TodoSeeder.php
+++ database/seeders/TodoSeeder.php
@@ -3,11 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\Todo;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 final class TodoSeeder extends Seeder
 {
-    private $todos;
+    private int $todoCount = 5;
+
+    private Collection $todos;
+
     /**
      * Run the database seeds.
      *
@@ -15,6 +19,6 @@ final class TodoSeeder extends Seeder
      */
     public function run()
     {
-        $this->todos = Todo::factory(5)->create();
+        $this->todos = Todo::factory($this->todoCount)->create();
     }
 }
